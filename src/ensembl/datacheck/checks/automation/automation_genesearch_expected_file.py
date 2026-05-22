201213f65d831614a4d1d1e294e173559800a84e
vinay <vinay@ebi.ac.uk>
2026-05-22 15:36:37 +0100

    add release to gene search

--- src/ensembl/datacheck/checks/automation/automation_genesearch_expected_file.py
+++ src/ensembl/datacheck/checks/automation/automation_genesearch_expected_file.py
@@ -39,7 +39,7 @@ def check_genesearch_expected_file(genomes, automation_resource_config):
     release_name = genomes.get("release_name")
     assert release_name is not None, f"Missing release_name for genome_uuid={genome_uuid}"
 
-    genesearch_file = Path(base_path)/ release_name / f"{genome_uuid}_toplevel_solr.json"
+    genesearch_file = Path(base_path)/ Path(f"release_{release_name}") / f"{genome_uuid}_toplevel_solr.json"
     assert genesearch_file.is_file(), (
         f"genesearch file does not exist for genome_uuid={genome_uuid}: {genesearch_file}"
     )
