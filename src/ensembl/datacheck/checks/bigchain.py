79289bcdaedf484f2f45c48e6af2f51d4ffd8001
Thomas Walsh <twalsh@ebi.ac.uk>
2026-06-02 09:18:53 +0100

    Add bigChain tests for nonzero entries and target coverage

--- src/ensembl/datacheck/checks/bigchain.py
+++ src/ensembl/datacheck/checks/bigchain.py
@@ -21,6 +21,8 @@ This module performs generic bigChain checks.
 Checks performed:
 1. check_exist: Asserts that the target bigChain path exists.
 2. check_validity: Asserts that the target file is readable as bigChain.
+3. check_nonzero_entries: Checks bigChain has at least one entry.
+4. check_nonzero_target_coverage: Checks bigChain has nonzero target coverage.
 """
 
 from contextlib import ExitStack
@@ -62,3 +64,49 @@ def check_validity(target_file: Path) -> None:
         except AttributeError as exc:
             raise AssertionError(f"Could not open target file as bigChain: {exc}") from exc
         assert is_bigbed, "The target file is not recognised as bigChain."
+
+
+def check_nonzero_entries(target_file: Path) -> None:
+    """
+    Check that the target assembly has at least one entry.
+
+    Args:
+        target_file: Path of target file.
+
+    Raises:
+        AssertionError: If the target file is unreadable, or
+            the target assembly has no entries.
+    """
+    with bb_bw_reader(target_file) as reader:
+        try:
+            target_chrom_sizes = reader.chroms()
+        except AttributeError as exc:
+            raise AssertionError(f"Could not open target file as bigChain: {exc}") from exc
+        entry_found = False
+        for chrom_id, chrom_size in target_chrom_sizes.items():
+            for _entry in reader.entries(chrom_id, 0, chrom_size):
+                entry_found = True
+                break
+            if entry_found:
+                break
+        assert entry_found, "Target assembly has no entries."
+
+
+def check_nonzero_target_coverage(target_file: Path) -> None:
+    """
+    Check that the target assembly has nonzero coverage.
+
+    Args:
+        target_file: Path of target file.
+
+    Raises:
+        AssertionError: If the target file is unreadable, or
+            the target assembly has no coverage.
+    """
+    with bb_bw_reader(target_file) as reader:
+        try:
+            bigchain_header = reader.header()
+        except AttributeError as exc:
+            raise AssertionError(f"Could not open target file as bigChain: {exc}") from exc
+        target_coverage = bigchain_header["nBasesCovered"]
+        assert target_coverage > 0, "Target assembly has no coverage."
