712cda314ceead2af9c6b8e360c0f7d65dd9ca09
Thomas Walsh <twalsh@ebi.ac.uk>
2026-06-30 12:54:16 +0100

    Add type hints to test_bigchain.py

--- tests/test_bigchain.py
+++ tests/test_bigchain.py
@@ -20,26 +20,27 @@ from pathlib import Path
 import re
 import shutil
 import subprocess
+from typing import Generator
 
 from pytest import fixture, raises
 
 from ensembl.datacheck.checks import bigchain
 
 
-def _setup_bigchain_file(bigbed_file_name):
+def _setup_bigchain_file(bigbed_file_name: str) -> Path:
     test_file_dir_path = Path(__file__).parent / "file" / "bigchain"
     return test_file_dir_path / bigbed_file_name
 
 
 @fixture(scope="module")
-def single_entry_bigchain_file():
+def single_entry_bigchain_file() -> Generator[Path, None, None]:
     """Single-entry bigChain file fixture."""
     single_entry_bigchain_path = _setup_bigchain_file("human_chimp.single.bb")
     yield single_entry_bigchain_path
 
 
 @fixture(scope="module")
-def truncated_bigchain_file():
+def truncated_bigchain_file() -> Generator[Path, None, None]:
     """Truncated bigChain file fixture."""
     single_entry_bigchain_path = _setup_bigchain_file("human_chimp.single.bb")
     truncated_bigchain_path = _setup_bigchain_file("human_chimp.truncated.bb")
@@ -50,7 +51,7 @@ def truncated_bigchain_file():
 
 
 @fixture(scope="module")
-def zero_entry_bigchain_file():
+def zero_entry_bigchain_file() -> Generator[Path, None, None]:
     """Empty bigChain file fixture."""
     empty_bigchain_path = _setup_bigchain_file("human_chimp.empty.bb")
     yield empty_bigchain_path
@@ -62,7 +63,7 @@ def test_check_exist_no_file():
         bigchain.check_exist("/path/to/nonexistent/file")
 
 
-def test_check_exist_zero_entry_bigchain_file(zero_entry_bigchain_file):
+def test_check_exist_zero_entry_bigchain_file(zero_entry_bigchain_file: Path):
     """Test bigChain existence check on zero-entry bigChain file."""
     bigchain.check_exist(zero_entry_bigchain_file)
 
@@ -80,39 +81,39 @@ def test_check_validity_non_bigchain_file():
         bigchain.check_validity(test_script_file_path)
 
 
-def test_check_validity_single_entry_bigchain_file(single_entry_bigchain_file):
+def test_check_validity_single_entry_bigchain_file(single_entry_bigchain_file: Path):
     """Test bigChain validity check on single-entry bigChain file."""
     bigchain.check_validity(single_entry_bigchain_file)
 
 
-def test_check_validity_truncated_bigchain_file(truncated_bigchain_file):
+def test_check_validity_truncated_bigchain_file(truncated_bigchain_file: Path):
     """Test bigChain validity check on truncated bigChain file."""
     with raises(ValueError, match="bigChain data accessibility"):
         bigchain.check_validity(truncated_bigchain_file)
 
 
-def test_check_validity_zero_entry_bigchain_file(zero_entry_bigchain_file):
+def test_check_validity_zero_entry_bigchain_file(zero_entry_bigchain_file: Path):
     """Test bigChain validity check on zero-entry bigChain file."""
     bigchain.check_validity(zero_entry_bigchain_file)
 
 
-def test_check_nonzero_entries_single_entry_bigchain_file(single_entry_bigchain_file):
+def test_check_nonzero_entries_single_entry_bigchain_file(single_entry_bigchain_file: Path):
     """Test bigChain nonzero entries check on single-entry bigChain file."""
     bigchain.check_nonzero_entries(single_entry_bigchain_file)
 
 
-def test_check_nonzero_entries_zero_entry_bigchain_file(zero_entry_bigchain_file):
+def test_check_nonzero_entries_zero_entry_bigchain_file(zero_entry_bigchain_file: Path):
     """Test bigChain nonzero entries check on zero-entry bigChain file."""
     with raises(AssertionError, match=re.escape("bigChain file has no entries.")):
         bigchain.check_nonzero_entries(zero_entry_bigchain_file)
 
 
-def test_check_nonzero_target_coverage_single_entry_bigchain_file(single_entry_bigchain_file):
+def test_check_nonzero_target_coverage_single_entry_bigchain_file(single_entry_bigchain_file: Path):
     """Test nonzero target coverage check on single-entry bigChain file."""
     bigchain.check_nonzero_target_coverage(single_entry_bigchain_file)
 
 
-def test_check_nonzero_target_coverage_zero_entry_bigchain_file(zero_entry_bigchain_file):
+def test_check_nonzero_target_coverage_zero_entry_bigchain_file(zero_entry_bigchain_file: Path):
     """Test nonzero target coverage check on zero-entry bigChain file."""
     with raises(AssertionError, match=re.escape("Target assembly has no coverage.")):
         bigchain.check_nonzero_target_coverage(zero_entry_bigchain_file)
